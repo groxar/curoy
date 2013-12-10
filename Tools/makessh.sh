69402bc0574180171be4d8a76a48d92ff171804d
groxar <groxar@gmail.com>
2013-12-10 12:57:29 +0100

    fixed makessh

--- Tools/makessh.sh
+++ Tools/makessh.sh
@@ -5,14 +5,15 @@
 host=teco 
 
 #script
-directory=$(readlink -fn -- "$1")
-name=$(basename $1)
+directory=$(readlink -fn -- "$(pwd)")
+name=$(basename $0)
 target=~${directory#$HOME}
 folder=$(dirname $target)
 
+
 if [ -d $1 ] ; then
-    ssh teco "mkdir -p $folder "
-    rsync -zr $1 $host:$folder 
+    ssh $host "mkdir -p $folder "
+    rsync -zr --partial --no-W $directory $host:$folder 
     ssh $host "cd $target && make -B" 
 else 
     echo "$1 is not valid";
