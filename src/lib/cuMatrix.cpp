0f76ecab01319d2c5b103ffeef59d8fb57392e79
groxar <groxar@gmail.com>
2014-01-20 22:12:04 +0100

    fixed []

--- src/lib/cuMatrix.cpp
+++ src/lib/cuMatrix.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cstdint>
+#include <numeric>
 #include <iostream>
 
 using namespace std;
@@ -9,37 +10,43 @@ using namespace std;
 template<typename T, size_t first, size_t... N>
 class cuMatrix{
 	public:
-		cuMatrix(T* matrix) :m_numDim(sizeof...(N)+1), m_matrix(matrix),m_arrDim{first,N...} {};
-		size_t getNumDim(){return m_numDim;};
+		cuMatrix(T* matrix) : m_matrix(matrix),m_arrDim{first,N...} {};
+		size_t getNumDim(){return m_arrDim.size();};
 		vector<size_t> getDim(){return m_arrDim;};
 
-		cuMatrix<T,N...> operator[](size_t n){return cuMatrix<T,N...>(m_matrix+m_arrDim[sizeof...(N)]*n);}; 
-
+		cuMatrix<T,N...> operator[](size_t n){
+			size_t memJump = 1;
+			auto end = m_arrDim.end();
+			for(auto i = (++m_arrDim.begin()); i != end; ++i)
+				memJump*= *i;
+			return cuMatrix<T,N...>(m_matrix+n*memJump);
+		}; 
 	private:
 		T* m_matrix;
-		size_t m_numDim;
 		vector<size_t> m_arrDim;
 };
 
 template<typename T, size_t first>
 class cuMatrix<T,first>{
 	public:
-		cuMatrix(T* matrix) :m_numDim(1), m_matrix(matrix),m_arrDim{first} {};
-		size_t getNumDim(){return m_numDim;};
+		cuMatrix(T* matrix) : m_matrix(matrix),m_arrDim{first} {};
+		size_t getNumDim(){return 1;};
 		vector<size_t> getDim(){return m_arrDim;};
 
 		T operator[](size_t n){return *(m_matrix+n);}; 
 
 	private:
 		T* m_matrix;
-		size_t m_numDim;
 		vector<size_t> m_arrDim;
 };
 
 int main(void){
 	int data[2][3] = {{1,2,3},{2,3,4}};
+	int data2[2][2][3] ={{{1,2,3},{4,5,6}}, {{7,8,9},{10,11,12}}};
 	int* dataP = (int*)data;
+	int* dataP2 = (int*)data2;
 	cuMatrix<int,2,3> matrix(dataP);
+	cuMatrix<int,2,2,3> matrix2(dataP2);
 
 	cout <<"getNumDim(): "<< matrix.getNumDim()<< endl;
 
@@ -48,6 +55,7 @@ int main(void){
 		cout << i << endl;
 
 	cout << "[1].getNumDim(): "<< matrix[1].getNumDim()<< endl;
+	cout << "2[1].getNumDim(): "<< matrix2[1].getNumDim()<< endl;
 	cout << "[0].getDim()[0]: "<< matrix[0].getDim()[0]<< endl;
 	cout << "[0][0]: "<< matrix[0][0]<< endl;
 	cout << "[0][1]: "<< matrix[0][1]<< endl;
@@ -55,6 +63,8 @@ int main(void){
 	cout << "[1][0]: "<< matrix[1][0]<< endl;
 	cout << "[1][1]: "<< matrix[1][1]<< endl;
 	cout << "[1][2]: "<< matrix[1][2]<< endl;
+	cout << "2[1][0][2]: "<< matrix2[1][1][2]<< endl;
+	cout << "2[1][1].getNumDim(): "<< matrix2[1][1].getNumDim()<< endl;
 	return 0;
 }
 
