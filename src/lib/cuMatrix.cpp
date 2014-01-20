bc9d2b3fe80b132c0f3d0992ea542dbc7587e772
groxar <groxar@gmail.com>
2014-01-20 20:52:54 +0100

    i'm insane

--- src/lib/cuMatrix.cpp
+++ src/lib/cuMatrix.cpp
@@ -4,15 +4,31 @@
 
 using namespace std;
 
-template<typename T, size_t... N>
+
+
+template<typename T, size_t first, size_t... N>
 class cuMatrix{
 	public:
-		cuMatrix(T* matrix) :m_numDim(sizeof...(N)), m_matrix(matrix),m_arrDim{N...} {};
+		cuMatrix(T* matrix) :m_numDim(sizeof...(N)+1), m_matrix(matrix),m_arrDim{first,N...} {};
 		size_t getNumDim(){return m_numDim;};
 		vector<size_t> getDim(){return m_arrDim;};
-		T getData(size_t n) {return m_matrix[n];};
 
-		cuMatrix<T,sizeof...(N)-1> operator[](size_t n){return cuMatrix<T,sizeof...(N)-1>(m_matrix+m_arrDim[sizeof...(N)-1]*n);}; 
+		cuMatrix<T,N...> operator[](size_t n){return cuMatrix<T,N...>(m_matrix+m_arrDim[sizeof...(N)]*n);}; 
+
+	private:
+		T* m_matrix;
+		size_t m_numDim;
+		vector<size_t> m_arrDim;
+};
+
+template<typename T, size_t first>
+class cuMatrix<T,first>{
+	public:
+		cuMatrix(T* matrix) :m_numDim(1), m_matrix(matrix),m_arrDim{first} {};
+		size_t getNumDim(){return m_numDim;};
+		vector<size_t> getDim(){return m_arrDim;};
+
+		T operator[](size_t n){return *(m_matrix+n);}; 
 
 	private:
 		T* m_matrix;
@@ -33,12 +49,12 @@ int main(void){
 
 	cout << "[1].getNumDim(): "<< matrix[1].getNumDim()<< endl;
 	cout << "[0].getDim()[0]: "<< matrix[0].getDim()[0]<< endl;
-	cout << "[0].getData(0): "<< matrix[0].getData(0)<< endl;
-	cout << "[0].getData(1): "<< matrix[0].getData(1)<< endl;
-	cout << "[0].getData(2): "<< matrix[0].getData(2)<< endl;
-	cout << "[1].getData(0): "<< matrix[1].getData(0)<< endl;
-	cout << "[1].getData(1): "<< matrix[1].getData(1)<< endl;
-	cout << "[1].getData(2): "<< matrix[1].getData(2)<< endl;
+	cout << "[0][0]: "<< matrix[0][0]<< endl;
+	cout << "[0][1]: "<< matrix[0][1]<< endl;
+	cout << "[0][2]: "<< matrix[0][2]<< endl;
+	cout << "[1][0]: "<< matrix[1][0]<< endl;
+	cout << "[1][1]: "<< matrix[1][1]<< endl;
+	cout << "[1][2]: "<< matrix[1][2]<< endl;
 	return 0;
 }
 
