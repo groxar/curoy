754f3d5753278cbaed2b9c6b167ea9a67d3cacab
groxar <groxar@gmail.com>
2014-01-27 23:20:33 +0100

    cuMatrixUnit test created

---
+++ src/unitTests/cuMatrixUnit.cpp
@@ -0,0 +1,35 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <iostream>
+#include "catch.hpp"
+#include "../lib/cuMatrix.hpp"
+
+using namespace std;
+TEST_CASE( "cuda matrix unit test", "[cuMatrix]" ){ 
+	
+	int data1[4][5] = {{1,2,3,4,5},{4,5,6,7,8},{7,8,9,10,11},{10,11,12,13,14}};
+	cuMatrix<int> dMatrix;
+	xMatrix<int> hMatrix((int*)data1,{4,5});
+	xMatrix<int> result;
+	cuMatrix<int> addResult;
+
+	SECTION("Data transfer from host to gpu and back"){
+		hMatrix >> dMatrix;
+		result << dMatrix;
+		REQUIRE(result[0][0] == 1);
+		REQUIRE(result[0][4] == 5);
+		REQUIRE(result[2][3] == 10 );
+		REQUIRE(result[3][0] == 10 );
+		REQUIRE(result[3][4] == 14 );
+	}
+	
+	SECTION("operator +"){ //every section seems to run with its own TEST_CASE initialization
+		hMatrix >> dMatrix;
+		addResult = dMatrix + dMatrix +dMatrix; 
+		result << addResult;
+		REQUIRE(result[0][0] == 3);
+		REQUIRE(result[0][4] == 15);
+		REQUIRE(result[2][3] == 30 );
+		REQUIRE(result[3][0] == 30 );
+		REQUIRE(result[3][4] == 42 );
+	}
+}
--- src/unitTests/xMatrixUnit.cpp
+++ src/unitTests/xMatrixUnit.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 #include "../lib/xMatrix.hpp"
 
-TEST_CASE( "matrix unit test", "[matrix]" ) {
+TEST_CASE( "cpu x86 matrix unit test", "[xMatrix]" ) {
 	int data[2][3] = {{1,2,3},{4,5,6}};
 	int data1[4][5] = {{1,2,3,4,5},{4,5,6,7,8},{7,8,9,10,11},{10,11,12,13,14}};
 	int data2[2][2][3] = 	{{{1,2,3},{4,5,6}}, 
