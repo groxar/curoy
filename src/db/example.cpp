1b98962088684585c865c9ca9f665f1f1f0c348d
Matthias Harrer <ma.harrer@gmail.com>
2014-02-25 16:16:10 +0100

    adjusted makefile; provided example for the use of the xMatrixRedisStringAdapter

--- src/db/example.cpp
+++ src/db/example.cpp
@@ -2,78 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <typeinfo>
+
 #include <hiredis.h>
 #include "../lib/xMatrix.hpp"
+#include "IxMatrixIOAdapter.hpp"
+#include "xMatrixRedisStringAdapter.hpp"
 
-int main(int argc, char **argv) {
-    unsigned int j;
-    redisContext *c;
-    redisReply *reply;
-    const char *socketpath = (argc > 1) ? argv[1] : "/tmp/redis.sock";
-    int port = (argc > 2) ? atoi(argv[2]) : 6379;
+using namespace curoy;
 
-    struct timeval timeout = { 1, 500000 }; // 1.5 seconds
-    c = (redisContext *) redisConnectUnixWithTimeout(socketpath, timeout);
-    if (c == NULL || c->err) {
-        if (c) {
-            printf("Connection error: %s\n", c->errstr);
-            redisFree(c);
-        } else {
-            printf("Connection error: can't allocate redis context\n");
+void print2d(xMatrix<double> matrix){
+    if(matrix.nDim() == 2){
+        for(int i = 0; i < matrix.dim(0); ++i){
+            for(int j = 0; j < matrix.dim(1); ++j){
+                printf("%f\t", (double) matrix[i][j]);
+            }
+            printf("\n");
         }
-        exit(1);
+    }else{
+        printf("not two dimensional!\n");
     }
+}
 
-    /* PING server */
-    reply = (redisReply *) redisCommand(c,"PING");
-    printf("PING: %s\n", reply->str);
-    freeReplyObject(reply);
 
-    /* Set a key */
-    reply = (redisReply *) redisCommand(c,"SET %s %s", "foo", "hello world");
-    printf("SET: %s\n", reply->str);
-    freeReplyObject(reply);
-
-    /* Set a key using binary safe API */
-    reply = (redisReply *) redisCommand(c,"SET %b %b", "bar", (size_t) 3, "hello", (size_t) 5);
-    printf("SET (binary API): %s\n", reply->str);
-    freeReplyObject(reply);
-
-    /* Try a GET and two INCR */
-    reply = (redisReply *) redisCommand(c,"GET foo");
-    printf("GET foo: %s\n", reply->str);
-    freeReplyObject(reply);
-
-    reply = (redisReply *) redisCommand(c,"INCR counter");
-    printf("INCR counter: %lld\n", reply->integer);
-    freeReplyObject(reply);
-    /* again ... */
-    reply = (redisReply *) redisCommand(c,"INCR counter");
-    printf("INCR counter: %lld\n", reply->integer);
-    freeReplyObject(reply);
-
-    /* Create a list of numbers, from 0 to 9 */
-    reply = (redisReply *) redisCommand(c,"DEL mylist");
-    freeReplyObject(reply);
-    for (j = 0; j < 10; j++) {
-        char buf[64];
-
-        snprintf(buf,64,"%d",j);
-        reply = (redisReply *) redisCommand(c,"LPUSH mylist element-%s", buf);
-        freeReplyObject(reply);
-    }
-
-    /* Let's check what we have inside the list */
-    reply = (redisReply *) redisCommand(c,"LRANGE mylist 0 -1");
-    if (reply->type == REDIS_REPLY_ARRAY) {
-        for (j = 0; j < reply->elements; j++) {
-            printf("%u) %s\n", j, reply->element[j]->str);
-        }
-    }
-    freeReplyObject(reply);
-
-    /* Disconnects and frees the context */
-    redisFree(c);
-
-    return 0;
+int main(int argc, char **argv)
+{
+	double data[2][3] = {{1.39587453768237689,2,3},{4,5,6}};
+	xMatrix<double> matrix((double*)data,{2,3});
+	IxMatrixIOAdapter *ioAdapter = new xMatrixRedisStringAdapter("/tmp/redis.sock");
+	ioAdapter->Save("test", matrix);
+    xMatrix<double> matrixPtr = ioAdapter->Load("test");
+    print2d(matrixPtr);
+	delete ioAdapter;
 }
