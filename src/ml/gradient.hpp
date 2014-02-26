30fdc42cec31ff670561388cc196139b9dbf82e3
groxar <groxar@gmail.com>
2014-02-26 18:21:00 +0100

    removed cast

--- src/ml/gradient.hpp
+++ src/ml/gradient.hpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 namespace curoy{
-	cuMatrix gradientDescent(cuMatrix<double> X, cuMatrix<double> y, cuMatrix<double> theta,double alpha, size_t num_iters){
+	cuMatrix<double> gradientDescent(const cuMatrix<double>& X, const cuMatrix<double>& y, cuMatrix<double>& theta ,const double alpha,  size_t numIterations){
 		size_t numDatasets = X.dim(0);	
+
+		for(size_t i = 0; i < numIterations;++i){
+			cout <<"within: "<< (cuMatrix<double>)(mult(X,theta)-y) * (cuMatrix<double>)X << endl;
+			//theta = (theta - (sum(T((mult(X,theta) - y) * X), 1) * (alpha * (1/numDatasets))));
+		}
+
+		return theta;
 	}
 }
