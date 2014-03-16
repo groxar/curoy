0c0625f05d57c3ffd98c48beafc82816e8eacaca
groxar <groxar@gmail.com>
2014-03-17 00:41:59 +0100

    rework ann

--- src/ml/gradient.hpp
+++ src/ml/gradient.hpp
@@ -5,10 +5,18 @@ using namespace std;
 
 namespace curoy{
 
-	template<typename FUNC>
- 	void gradientDescent(FUNC f,const cuMatrix<double>& X, const cuMatrix<double>& y, cuMatrix<double>& theta ,const double alpha,  size_t numIterations){
+	//TODO FUNCTIONIZE
+	/*
+ 	void gradientDescentAnn(const cuMatrix<double>& X, const cuMatrix<double>& y, ann& myAnn,const double alpha, const double lambda,  size_t numIterations){
 		size_t numDataSets= X.dim(0);
+
+		for(size_t i = 0; i<numIterations;++i){
+			auto gradient= myAnn.gradientFunction(X,y,lambda);
+			myAnn.hTheta -= alpha * get<0>(gradient);
+			myAnn.oTheta -= alpha * get<1>(gradient);
+		}
 	}
+	*/
 
 	//logistic or linear regression; TODO look up what it was!!
  	void gradientDescent(const cuMatrix<double>& X, const cuMatrix<double>& y, cuMatrix<double>& theta ,const double alpha,  size_t numIterations){
