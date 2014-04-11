8481d3a0a31d40bfd541aaa048ff9b016f0a42e5
groxar <groxar@gmail.com>
2014-04-12 01:02:43 +0200

    fix

--- src/unitTests/annUnit.cpp
+++ src/unitTests/annUnit.cpp
@@ -83,7 +83,8 @@ TEST_CASE("[ann]", "cuda artifical neural network"){
 		ann myAnn(400,10,{25});
 		cout << sum(Y)<<endl;
 		startChrono();
-		myAnn.conjugateDescent(X,Y,1,4000);
+		myAnn.conjugateDescent(X,Y,1,500);
+		//myAnn.gradientDescent(X,Y,0.2,1,500);
 		timeChrono("gradienDescent");
 		xMatrix<double> out;
 		out << myAnn.hiddenLayerVec[0];
@@ -91,8 +92,6 @@ TEST_CASE("[ann]", "cuda artifical neural network"){
 		out << myAnn.hiddenLayerVec[1];
 		writeFile(out,"hl1");
 		cout << myAnn.predict(X)<<endl;
-		printGpuMem();
-
 
 		cudaDeviceReset();
 	}
