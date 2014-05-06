6fb27f52069134739aa1ba6b3ccf3733ebe04cb9
Matthias Harrer <ma.harrer@gmail.com>
2014-05-06 12:53:03 +0200

    fixed wavelet decomposition

--- src/wt/example.cpp
+++ src/wt/example.cpp
@@ -1,16 +1,48 @@
 #include "xWaveletTransform.hpp"
+#include "xFilter.hpp"
+#include "waveletReturn.hpp"
 #include <iostream>
+#include <vector>
 
 using namespace curoy;
+using namespace std;
 
 
 int main(int argc, char **argv)
 {
-    xWaveletTransform transformator;
-    double data[] = {6, 12, 15, 15, 14, 12, 120, 116};
-    double *transformedData = transformator.doHaarWaveletTransform(data, 8);
+    xFilter db4;
+    //db1 / haar
+    double hi[] = {1, -1};
+    double lo[] = {1, 1};
+    //db2
+    //double hi[] = {-0.1830127, -0.3169873, 1.1830127, -0.6830127};
+    //double lo[] = {0.6830127, 1.1830127, 0.3169873, -0.1830127};
+    //db3
+    //double hi[] = {0.0498175, 0.12083221, -0.19093442, -0.650365, 1.14111692, -0.47046721};
+    //double lo[] = {0.47046721, 1.14111692, 0.650365, -0.19093442, -0.12083221, 0.0498175};
+    //coif1
+    //double lo[] = {-0.1028594569415370, 0.4778594569415370, 1.2057189138830700, 0.5442810861169260, -0.1028594569415370, -0.0221405430584631};
+    //double hi[] = {-0.0221405430584631, 0.1028594569415370, 0.5442810861169260, -1.2057189138830700, 0.4778594569415370, 0.1028594569415370};
+    db4.hiFilterCoeff = hi;
+    db4.loFilterCoeff = lo;
+    db4.length = 2;
 
-    for(int i=0; i<8; ++i){
-    	std::cout << transformedData[i] << " ";
+    double data[] = {345,564,3,234,56,576,324,6,345,564,3,234,56,576,324,6,345,564,3,234,56,576,324,6};
+    xWaveletTransform transformator;
+
+    //WaveletReturn transformedData;
+    //for(int i = 0; i < 10000; ++i)
+    //{
+        WaveletReturn* transformedData = transformator.doWaveletTransform(data, 24, 3, db4);
+    //}
+
+    for(vector<size_t>::iterator it = transformedData->levelLengths.begin(); it != transformedData->levelLengths.end(); it++){
+        cout << *it << " ";
     }
+    cout << endl;
+    cout << transformedData->totalLength << ": ";
+    for(int i=0; i<transformedData->totalLength; ++i){
+    	cout << transformedData->data[i] << ", ";
+    }
+    cout << endl;
 }
--- src/wt/xWaveletTransform.cpp
+++ src/wt/xWaveletTransform.cpp
@@ -1,29 +1,119 @@
 #include "xWaveletTransform.hpp"
+#include <math.h>
+#include <iostream>
+#include <vector>
 
+using namespace std;
 namespace curoy{
-		double* xWaveletTransform::doHaarWaveletTransform(const double* data, size_t length)
-		{
-			double* transformedData = new double[length];
-			double* temp = new double[length];
+    WaveletReturn* xWaveletTransform::doWaveletTransform(const double* data, size_t length, size_t level, xFilter filter)
+    {
+        size_t totalLength = 0;
+        vector<WaveletReturn*> waveletReturns;
 
-			for(size_t i = 0; i < length; ++i)
-			{
-				transformedData[i] = data[i];
-			}
+        vector<size_t> returnLengths;
+        WaveletReturn* toReturn = new WaveletReturn();
+        toReturn->levelLengths = returnLengths;
 
-			for(size_t half = length >> 1; half > 0; half = half >> 1)
-			{
-				for (size_t i = 0; i < half; i++)
-	            {
-	                size_t k = (i << 1);
-	                temp[i] = (transformedData[k] + transformedData[k + 1]) / 2;
-	                temp[i + half] = (transformedData[k] - transformedData[k + 1]) / 2;
-	            }
-	            for(size_t i = 0; i < 2 * half; i++)
-	            {
-	            	transformedData[i] = temp[i];
-	            }
-			}
-			return transformedData;
-		}
+        const double* temp = data;
+
+        size_t currentLength = length;
+        for(size_t curLevel = 0; curLevel < level; curLevel++)
+        {
+            WaveletReturn *waveletReturn = doOneLevelWaveletTransform(temp, currentLength, filter);
+            waveletReturns.insert(waveletReturns.begin(), waveletReturn);
+
+            temp = waveletReturn->data;
+
+            currentLength = waveletReturn->levelLengths[0];
+            totalLength += currentLength;
+            if(curLevel == level - 1){
+                totalLength += currentLength;
+            }
+        }
+        toReturn->data = new double[totalLength];
+        toReturn->totalLength = totalLength;
+        int writtenToData = 0;
+        for(int i = 0; i < waveletReturns.size(); i++)
+        {
+            WaveletReturn* currentWaveletReturn = waveletReturns[i];
+            if(i == 0)
+            {
+                toReturn->levelLengths.push_back(currentWaveletReturn->levelLengths[0]);
+                toReturn->levelLengths.push_back(currentWaveletReturn->levelLengths[1]);
+                for(int j = 0; j < currentWaveletReturn->levelLengths[0]; ++j)
+                {
+                    toReturn->data[writtenToData++] = currentWaveletReturn->data[j];
+                }
+            }
+            else if(i > 0)
+            {
+                toReturn->levelLengths.push_back(currentWaveletReturn->levelLengths[1]);
+            }
+
+            for(int j = currentWaveletReturn->levelLengths[0]; j < currentWaveletReturn->levelLengths[0] + currentWaveletReturn->levelLengths[1]; ++j)
+            {
+               toReturn->data[writtenToData++] = currentWaveletReturn->data[j];
+            }
+        }
+        toReturn->levelLengths.push_back(length);
+
+        for(int i = 0; i < waveletReturns.size(); i++)
+        {
+            delete waveletReturns[i];
+        }
+
+        return toReturn;
+    }
+
+    WaveletReturn* xWaveletTransform::doOneLevelWaveletTransform(const double* data, size_t length, xFilter filter)
+    {
+        size_t resultLength = ((length + length % 2) / 2 + filter.length / 2 - 1) * 2;
+        double* preparedData = new double[resultLength + filter.length - 2];
+        //prepare Data
+        for(int i = 0; i < resultLength + filter.length - 2; i++)
+        {
+            int actualIndex = i - (filter.length - 2);
+            if(actualIndex >= 0 && actualIndex < (int)length){
+                preparedData[i] = data[actualIndex];
+            }
+            else if(actualIndex >= (int)length)
+            {
+                preparedData[i] = data[2 * length - actualIndex - 1];
+            }
+            else if(actualIndex < 0)
+            {
+                preparedData[i] = data[abs(actualIndex) - 1];
+            }
+        }
+        double* result = new double[resultLength];
+        size_t half = resultLength >> 1;
+
+        for(int i = 0; i < half; i++)
+        {
+            size_t k = (i << 1);
+
+            double loFilterSum = 0;
+            double hiFilterSum = 0;
+            //loop over filter length
+            for(int j = 0; j < filter.length; j++)
+            {
+                loFilterSum += preparedData[(k + j)] * filter.loFilterCoeff[j];
+                hiFilterSum += preparedData[(k + j)] * filter.hiFilterCoeff[j];
+            }
+
+            result[i] = loFilterSum / sqrt(2.0);
+            result[i + half] = hiFilterSum / sqrt(2.0);
+        }
+
+        delete[] preparedData;
+        WaveletReturn *waveletReturn = new WaveletReturn();
+        waveletReturn->data = result;
+        waveletReturn->totalLength = resultLength;
+        vector<size_t> lengths;
+        lengths.push_back(half);
+        lengths.push_back(half);
+        waveletReturn->levelLengths = lengths;
+
+        return waveletReturn;
+    }
 }
